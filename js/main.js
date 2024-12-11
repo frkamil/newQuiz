8cfc70de9fb04223db30c8f2704f37180f9cb7b9
frkamil <144934172+frkamil@users.noreply.github.com>
2024-12-10 22:23:37 -0700

    Update main.js
    Number only answers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Random Questions Quiz
 document.getElementById("quizMark").style.display = "none";
 
-//Initalize Questions
+//Initialize Questions
 let questionArray = [];
 questionArray.push(
   new Question("What is the most common surname in the United States?", "smith")
@@ -21,7 +21,7 @@ questionArray.push(
 questionArray.push(
   new Question("What fish is used to make calamari?", "squid")
 );
-questionArray.push(new Question("How many Great Lakes are there?", "five"));
+questionArray.push(new Question("How many Great Lakes are there? Please write your answer as a number", "5"));
 questionArray.push(
   new Question("Can you really see the Great Wall of China from space?", "no")
 );
@@ -43,7 +43,7 @@ questionArray.push(
 questionArray.push(
   new Question("What month was Julius Caesar stabbed in?", "march")
 );
-questionArray.push(new Question("How many bones do sharks have?", "none"));
+questionArray.push(new Question("How many bones do sharks have? Please write your answer as a number.", "0"));
 
 //Start Quiz!
 
